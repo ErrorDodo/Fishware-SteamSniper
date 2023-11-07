19d199c1891f4bfc9d8cde2338b47dbc1be26029
Dylan <41908416+ErrorDodo@users.noreply.github.com>
2023-11-07 22:02:56 +0800

    Replace monorepo with simple console app

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import axios from "axios";
+import { WebHelper } from "./SteamHelper";
+
+const webHelper = new WebHelper(axios);
+const profileUrls = [
+  "https://steamcommunity.com/id/tedlookatmyprofile",
+  "https://steamcommunity.com/id/dodowhatyouwantcauseapiratelifeisfree",
+];
+
+const checkProfiles = async () => {
+  console.log("Running a check on the profiles...");
+  try {
+    const profilesExist = await webHelper.doProfilesExist(profileUrls);
+    profilesExist.forEach((exists, index) => {});
+  } catch (error) {}
+};
+
+// Run the task every 5 seconds
+setInterval(checkProfiles, 5000);
+
+console.log("Task scheduled. Checking profiles every 5 seconds.");
