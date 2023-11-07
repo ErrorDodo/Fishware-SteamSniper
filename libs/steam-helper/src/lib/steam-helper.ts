7c957025cb7ad3fec3f3a7eec2541d5a957e2eb3
Dylan <41908416+ErrorDodo@users.noreply.github.com>
2023-11-07 21:52:21 +0800

    Refactor WebHelper class to check multiple profiles at once.

--- libs/steam-helper/src/lib/steam-helper.ts
+++ libs/steam-helper/src/lib/steam-helper.ts
@@ -1,9 +1,10 @@
-import { Logger, ConsoleLogger } from './ConsoleLogger';
-import axios from 'axios';
-import * as cheerio from 'cheerio';
+import { Logger, ConsoleLogger } from "./ConsoleLogger";
+import axios from "axios";
+import * as cheerio from "cheerio";
 
 export interface IWebHelper {
-  doesProfileExist(url: string): Promise<boolean>;
+  // doesProfileExist(url: string): Promise<boolean>;
+  doProfilesExist(urls: string[]): Promise<boolean[]>;
 }
 
 export class WebHelper implements IWebHelper {
@@ -15,29 +16,46 @@ export class WebHelper implements IWebHelper {
     this.logger = logger ?? new ConsoleLogger();
   }
 
-  public async doesProfileExist(url: string): Promise<boolean> {
+  private async doesProfileExist(url: string): Promise<boolean> {
     try {
       const response = await this.httpClient.get(url);
       const htmlCode = response.data;
       const $ = cheerio.load(htmlCode);
 
       // Using CSS selectors to locate the specific HTML structure
-      const node = $('#BG_bottom #message h3');
+      const node = $("#BG_bottom #message h3");
       if (
         node.length &&
-        node.text().includes('The specified profile could not be found.')
+        node.text().includes("The specified profile could not be found.")
       ) {
         this.logger.warn(`Profile not found at URL: ${url}`);
-        // The specific HTML block exists, which means the profile does not exist
         return false;
       }
-      // If the block does not exist, then we can assume the profile does exist
+      this.logger.info(`Profile found at URL: ${url}`);
       return true;
     } catch (ex) {
       this.logger.error(
         `An error occurred while checking the profile at URL: ${url}`
       );
-      throw new Error('An error occurred while checking the profile.');
+      // Rethrow the error to handle it in the calling code
+      throw ex;
     }
+  }
+
+  public async doProfilesExist(urls: string[]): Promise<boolean[]> {
+    // Map each URL to the doesProfileExist promise
+    const profileExistencePromises = urls.map((url) =>
+      this.doesProfileExist(url).catch((error) => {
+        // Log the error and return false as the default "existence" state in case of error
+        this.logger.error(
+          `An error occurred while checking the profile at URL: ${url}: ${error}`
+        );
+        return false;
+      })
+    );
+
+    // Execute all the promises concurrently
+    const profilesExistence = await Promise.all(profileExistencePromises);
+    return profilesExistence;
   }
 }
